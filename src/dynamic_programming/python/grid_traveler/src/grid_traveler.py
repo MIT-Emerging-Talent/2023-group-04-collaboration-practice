daac36630f1defb2478e0cb8bc3bae3fd8cf0d95
sediqem <147769783+sediqem@users.noreply.github.com>
2024-01-19 15:34:35 -0500

    Update grid_traveler.py

--- src/dynamic_programming/python/grid_traveler/src/grid_traveler.py
+++ src/dynamic_programming/python/grid_traveler/src/grid_traveler.py
@@ -1,6 +1,14 @@
+
+"""This is a recursive function that finds the number
+of paths,by considering the number of paths to the
+top left corner of the grid and the number of paths
+to the left of the current position."""
+# number of row is "a", number of coloumn is b
 def grid(a,b):
     if a == 1:
         return 1
     if b == 1:
         return 1
+    #the number of steps to the left is "a-1"
+    #the number of steps to down are "b-1"
     return grid(a-1, b) + grid(a, b-1)
